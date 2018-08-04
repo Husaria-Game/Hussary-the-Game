7bdce63fa42a231f61b066c3bc35190867dce630
Grzegorz <geerex@outlook.com>
2018-08-04 12:52:30 +0200

    add game entry point - TacticsCard and UnitCard prefabs ready

---
+++ Assets/Scripts/Visual/CardDisplayLoader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplayLoader : MonoBehaviour {
+
+    public Card card;
+    [Header("Text References")]
+    public Text nameText;
+    public Text cardCostText;
+    public Text descriptionText;
+    public Text healthText;
+    public Text attackText;
+    [Header("Image References")]
+    public Image topRibbonImage;
+    public Image lowRibbonImage;
+    public Image profileImage;
+    public Image bodyImage;
+    public Image cardFaceFrameImage;
+    public Image cardFaceGlowImage;
+    public Image cardBackGlowImage;
+
+
+    // Use this for initialization
+    void Start () {
+        if(card != null) loadCardAsset();
+	}
+
+    private void loadCardAsset()
+    {
+        nameText.text = card.name;
+        cardCostText.text = card.cardCost.ToString();
+        descriptionText.text = card.description;
+        if (card.maxHealth > 0)
+        {
+            healthText.text = card.maxHealth.ToString();
+            attackText.text = card.attack.ToString();
+        }
+
+        // load card color based on affiliation
+        if(card.affiliation == Affiliation.Poland)
+        {
+            //topRibbonImage.color = card.;
+            //lowRibbonImage;
+            //profileImage;
+            //bodyImage.color = ;
+        }
+        else if(card.affiliation == Affiliation.Ottoman)
+        {
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Visual/CardRotation.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class CardRotation : MonoBehaviour {
+
+    //public float moveSpeed = 2f;
+    //public float turnSpeed = 100f;
+
+    //// Use this for initialization
+    //void Start()
+    //{
+
+    //}
+
+    //void Update()
+    //{
+    //    if (Input.GetMouseButton(0))
+    //    {
+    //        //transform.Rotate(new Vector3(2, 1, 0) * turnSpeed * Time.deltaTime);
+    //        //transform.Translate(new Vector3(2, 1, 0) * moveSpeed * Time.deltaTime);
+    //        Vector3 b = transform.forward;
+    //        Debug.Log("position " + (transform.position));
+    //        Debug.Log("rotation " + (transform.rotation));
+    //    }
+    //    if (Input.GetMouseButton(1))
+    //    {
+    //        transform.Rotate(new Vector3(2, 1, 0) * -turnSpeed * Time.deltaTime);
+    //        transform.Translate(new Vector3(-2, -1, 0) * moveSpeed * Time.deltaTime);
+    //    }
+
+    //}
+
+    [SerializeField]
+    private GameObject cardFace;
+
+    [SerializeField]
+    private GameObject cardBack;
+
+    protected void Update()
+    {
+        //// camera position relative to object
+        //Vector3 a = (Camera.main.transform.position - transform.position);
+        //// moving object backwards
+        //Vector3 b = -transform.forward;
+        //float rotAngle = (Vector3.Dot(a, b) / (a.magnitude * b.magnitude)) * Mathf.Rad2Deg;
+        bool back = Vector3.Dot(Camera.main.transform.forward, transform.forward) < 0;
+        //bool back = rotAngle < 0;
+        cardFace.SetActive(!back);
+        cardBack.SetActive(back);
+        //Debug.Log("CAMERA" + Camera.main.transform.position);
+        //Debug.Log("OBJECT" + (-transform.position));
+        //Debug.Log("SUM" + (Camera.main.transform.position - transform.position));
+        //Debug.Log("OBJECT forward" + (transform.forward));
+        //Debug.Log("A Magnitude " + a.magnitude);
+        //Debug.Log("B Magnitude " + b.magnitude);
+        //Debug.Log("A * B Magnitude " + (a.magnitude * b.magnitude));
+        //Debug.Log("DOT " + (Vector3.Dot(a, b)));
+        //Debug.Log("DOT_2 " + (Vector3.Dot(Camera.main.transform.forward, transform.forward)));
+        //Debug.Log("------------------------------------------------");
+    }
+}
