a6924f5608927273b6f5e5b381f8999718db813b
Grzegorz <geerex@outlook.com>
2018-08-05 23:00:35 +0200

    added new scene to test dragging, card prefabs remastered, implemented tweening engine

---
+++ Assets/Scripts/UI/Draggable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Transform parentToReturnTo = null;
+    public Vector3 initialPosition;
+    public bool dragSuccess;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentToReturnTo = this.transform.parent;
+        initialPosition = this.transform.position;
+        Debug.Log("on begin drag");
+        dragSuccess = false;
+        //this.transform.SetParent(this.transform.parent.parent);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //this.transform.SetParent(parentToReturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        //transform.DOMove(parentToReturnTo, 1f);
+        if (dragSuccess)//for dragging successful
+        {
+        }
+        else
+        {
+            transform.DOMove(initialPosition, 1);
+            //this.transform.SetParent(parentToReturnTo);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/DropZone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        //Debug.Log("OnDrop to " + gameObject.name);
+        Debug.Log(eventData.pointerDrag.name + "was dropped to " + gameObject.name);
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if(d != null)
+        {
+            d.dragSuccess = true;
+            d.parentToReturnTo = this.transform;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerEnter to " + gameObject.name);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerExit to " + gameObject.name);
+    }
+}
