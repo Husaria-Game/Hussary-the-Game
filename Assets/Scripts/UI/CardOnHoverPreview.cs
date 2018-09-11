40365c49ee9b576a1d58e8675d26f3a6d4bdf920
Grzegorz <geerex@outlook.com>
2018-09-11 17:59:48 +0200

    added UnitCard and Unit prefabs, modified on hoverPreviews

--- Assets/Scripts/UI/CardOnHoverPreview.cs
+++ Assets/Scripts/UI/CardOnHoverPreview.cs
@@ -3,22 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CardOnHoverPreview : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class CardOnHoverPreview : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject previewGameObject;
+    private static CardOnHoverPreview cardInPreview = null;
 
+     public void OnPointerDown(PointerEventData eventData)
+    {
+        //disable all other previews
+        if(cardInPreview != null)
+        {
+            cardInPreview.previewGameObject.SetActive(false);
+        }
+        cardInPreview = this;
+
+        if (eventData.button == PointerEventData.InputButton.Right)
+            previewGameObject.SetActive(!previewGameObject.activeSelf);
+        //if (eventData.button == PointerEventData.InputButton.Left)
+        //    Debug.Log("Left click");
+        //else if (eventData.button == PointerEventData.InputButton.Middle)
+        //    Debug.Log("Middle click");
+        //else if (eventData.button == PointerEventData.InputButton.Right)
+        //    previewGameObject.SetActive(true);
+    }
     public void OnPointerEnter(PointerEventData eventData)
     {
         //Debug.Log("OnPointerEnter to " + gameObject.name);
-        previewGameObject.SetActive(true);
-
-        //gameObject.SetActive(false);
+        //Debug.Log("OnPointerEnter cardInPreview " + this);
+        //if (cardInPreview != null)
+        //{
+        //    cardInPreview.previewGameObject.SetActive(false);
+        //}
+        //cardInPreview = this;
+        //previewGameObject.SetActive(true);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         //Debug.Log("OnPointerExit to " + gameObject.name);
-        previewGameObject.SetActive(false);
-        //gameObject.SetActive(true);
+        //if (cardInPreview != null)
+        //{
+        //    cardInPreview.previewGameObject.SetActive(false);
+        //}
+        //Debug.Log("OnPointerExit cardInPreview " + this);
     }
 }
