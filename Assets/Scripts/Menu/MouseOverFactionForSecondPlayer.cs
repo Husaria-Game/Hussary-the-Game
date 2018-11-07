b215a7d02b212a12c92ed4f71b902ee6f656233e
Cristopher-91 <k.piasecki91@gmail.com>
2018-11-07 08:06:20 +0100

    All suggested changes made

---
+++ Assets/Scripts/Menu/MouseOverFactionForSecondPlayer.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class MouseOverFactionForSecondPlayer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public ChooseFactionForSecondPlayer CFS;
+    public Image imageThis;
+    public MouseOverFactionForSecondPlayer imageOther;
+    private bool isClicked = false;
+    private bool isBlocked = false;
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!isClicked)
+        {
+            imageThis.transform.localScale += new Vector3(0.25f, 0.25f, 0);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!isClicked)
+        {
+            imageThis.transform.localScale -= new Vector3(0.25f, 0.25f, 0);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            if (!isClicked)
+            {
+                isClicked = true;
+                CFS.isClicked = !CFS.isClicked;
+                CFS.setSecondFaction(imageThis.GetComponentInChildren<Text>().text);
+                UnclickOtherImage();
+                isBlocked = false;
+            }
+            else if (isClicked && !isBlocked)
+            {
+                isClicked = false;
+                CFS.isClicked = !CFS.isClicked;
+                isBlocked = true;
+            }
+        }
+    }
+
+    private void UnclickOtherImage()
+    {
+        if (imageOther.isClicked)
+        {
+            imageOther.imageThis.transform.localScale -= new Vector3(0.25f, 0.25f, 0);
+            CFS.isClicked = !CFS.isClicked;
+        }
+        imageOther.isClicked = false;
+    }
+}
