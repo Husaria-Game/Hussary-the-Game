fd9851fee91c23e35c06ec3c15009ce2a842f9fd
Cristopher-91 <k.piasecki91@gmail.com>
2018-11-14 10:12:36 +0100

    Script shows all cards - want to add possibility to choose specific faction

--- Assets/Scripts/Menu/CardEncyclopedia.cs
+++ Assets/Scripts/Menu/CardEncyclopedia.cs
@@ -7,48 +7,116 @@ using System.Linq;
 public class CardEncyclopedia : MonoBehaviour {
 
     private Object[] cards;
-    private List<Card> cardsToDisplay;
-    private List<Card> filteredCards;
+    private List<Card> allCards;
+    private List<Card> ottomanCards;
+    private List<Card> polandCards;
+
     public Text cardName;
     public Image cardImage;
     public Text cardHistoryDescription;
     public Dropdown factionMenu;
 
+    public Button nextCardButton;
+    public Button previousCardButton;
+
+    private int cardNumber = 0;
+
 	// Use this for initialization
 	void Start () {
 
-        cardsToDisplay = new List<Card>();
-        filteredCards = new List<Card>();
-
+        allCards = new List<Card>();
         cards = Resources.LoadAll("Cards", typeof(Card));
 
         foreach (Card card in cards)
         {
-            cardsToDisplay.Add(card);     
+            allCards.Add(card);
+            Debug.Log(card.cardName);
         }
+        /*
+        polandCards = allCards.Select(c => c).Where(c => c.affiliation == 
+        Affiliation.Poland);
 
-        cardName.text = cardsToDisplay[0].cardName;
-        cardImage.sprite = cardsToDisplay[0].cardImage;
-        cardHistoryDescription.text = cardsToDisplay[0].historyDescription;
+        ottomanCards = allCards.Select(c => c).Where(c => c.affiliation == 
+        Affiliation.Ottoman);
+        */
 
+        //Ustawienie Guzika do tyłu w stan wyłączenia
+        previousCardButton.enabled = false;
+        previousCardButton.GetComponentInChildren<Text>().text = "Brak";
+
+        //Załadowanie pierwszej karty
+        cardName.text = allCards[cardNumber].cardName;
+        cardImage.sprite = allCards[cardNumber].cardImage;
+        cardHistoryDescription.text = allCards[cardNumber].historyDescription;
+        
     }
-	
-	void Update () {
 
-		if(factionMenu.captionText.Equals("Wszystkie"))
+    public void showCards()
+    {
+        if (factionMenu.captionText.text == "Wszystkie")
         {
-            filteredCards = cardsToDisplay;
+            Debug.Log("All");
+
+            //showCard(allCards);
         }
-        else if(factionMenu.captionText.Equals("Rzeczpospolita Polska"))
+        else if (factionMenu.captionText.text == "Rzeczpospolita Polska")
         {
-            filteredCards = cardsToDisplay.Select(n=>n).Where(n.affiliation.equals)
+            Debug.Log("Poland");
+            //showCard(polandCards);
         }
-        else if (factionMenu.captionText.Equals("Imperium Osmańskie"))
+        else if (factionMenu.captionText.text == "Imperium Osmańskie")
         {
+            Debug.Log("Ottoman");
+            //showCard(ottomanCards);
+        }
+    }
 
+
+
+
+
+
+
+
+    public void loadCard(int cardNumber)
+    {
+        cardName.text = allCards[cardNumber].cardName;
+        cardImage.sprite = allCards[cardNumber].cardImage;
+        cardHistoryDescription.text = allCards[cardNumber].historyDescription;
+    }
+
+    public void nextCard()
+    {
+        if (cardNumber == allCards.Count - 2)
+        {
+            nextCardButton.enabled = false;
+            nextCardButton.GetComponentInChildren<Text>().text = "Brak"; 
         }
 
+            cardNumber++;
+            loadCard(cardNumber);
+            if(previousCardButton.enabled == false)
+            {
+                previousCardButton.enabled = true;
+                previousCardButton.GetComponentInChildren<Text>().text = "Poprzednia";
+            }
+    }
 
+    public void previousCard()
+    {
+        if(cardNumber == 1)
+        {
+            previousCardButton.enabled = false;
+            previousCardButton.GetComponentInChildren<Text>().text = "Brak";
+        }
+
+            cardNumber--;
+            loadCard(cardNumber);
+            if (nextCardButton.enabled == false)
+            {
+                nextCardButton.enabled = true;
+                nextCardButton.GetComponentInChildren<Text>().text = "Następna";
+            }
     }
 
 }
