e4924566c4d2b9aa82b778a96966e6675ec3354f
Cristopher91 <k.piasecki91@gmail.com>
2018-11-18 22:19:08 +0100

    Before Merging with master

---
+++ Assets/Scripts/Tactics/ASREffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ASREffect : MonoBehaviour {
+
+    private bool isEffectGoingToTakePlace;
+    public bool IsEffectiGoingToTakePlace { get; set; }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
