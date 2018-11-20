d04443999c13bc10bcbca572832f6ee9b57f9add
Cristopher-91 <k.piasecki91@gmail.com>
2018-11-20 13:23:01 +0100

    H-31 Added DebugTextBox

---
+++ Assets/Scripts/View/DebugMessege.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugMessege : MonoBehaviour {
+
+    public Image incomingTextImage;
+    public Text incomingText;
+
+    void Start() {
+        incomingText.enabled = false;
+        incomingTextImage.enabled = false;
+    }
+
+
+    public void ShowDebugText(string s)
+    {
+        StartCoroutine(ShowDebugTextWithDelay(s));
+    }
+
+    IEnumerator ShowDebugTextWithDelay(string s)
+    {       
+        incomingTextImage.enabled = true;
+        yield return new WaitForSeconds(2f);
+    
+        incomingText.text = s;
+        incomingText.enabled = true;
+        yield return new WaitForSeconds(1f);
+
+        incomingTextImage.enabled = false;
+        incomingText.enabled = true;
+        yield return new WaitForSeconds(5f);
+
+        incomingText.enabled = false;
+    }
+}
