34a5b7ad3e384caf5add100641d2c634b4907814
szczepaniakjm <szczepaniak.jm@gmail.com>
2018-11-21 00:33:48 +0100

    Some little features
    Distinction of fractions during drawing cards is done, some description of cards is added, a DropAreaImage cards limit added

--- Assets/Scripts/Model/ArmyModel.cs
+++ Assets/Scripts/Model/ArmyModel.cs
@@ -13,14 +13,14 @@ public class ArmyModel {
     {
         if (faction == Faction.Ottoman)
         {
-            this.armyCardsModel = new ArmyCardsModel();
+            this.armyCardsModel = new ArmyCardsModel(faction);
             this.heroModel = new HeroModel(0, "Sulejman", 20);
             //this.town = new Town(0, "Istambul", "greatest town", 40);
             //this.town.name.text = "Istanbul";
         }
         else
         {
-            this.armyCardsModel = new ArmyCardsModel();
+            this.armyCardsModel = new ArmyCardsModel(faction);
             this.heroModel = new HeroModel(0, "Jan III Sobieski", 20);
             //this.town = new Town(0, "Warsaw", "strongest town", 45);
         }
