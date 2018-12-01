63feebba832b1de3a2b1674051c6fd4ae4f328a0
G-Captain <grzekanowski@gmail.com>
2018-12-02 00:48:38 +0100

    H-69 cleanup of unnecessary code lines

--- Assets/Scripts/AI/AIManager.cs
+++ Assets/Scripts/AI/AIManager.cs
@@ -10,14 +10,8 @@ public class AIManager : MonoBehaviour
     public static AIManager Instance;
     public bool canAIMakeMove;
     public List<GameObject> playableCardList { get; set; }
-    public List<GameObject> playableUnitCardList { get; set; }
-    public List<GameObject> playableTacticsHealCardList { get; set; }
-    public List<GameObject> playableTacticsStrenghtCardList { get; set; }
-    public List<GameObject> playableTacticsAttackCardList { get; set; }
-//    public List<GameObject> tacticsCardList { get; set; }
     public List<GameObject> attackableUnitList { get; set; }
     public List<GameObject> defendableUnitList { get; set; }
-    public List<GameObject> friendlyUnitList { get; set; }
 
     void Awake()
     {
@@ -28,45 +22,33 @@ public class AIManager : MonoBehaviour
     {
         canAIMakeMove = false;
         this.playableCardList = new List<GameObject>();
-        this.playableUnitCardList = new List<GameObject>();
-        this.playableTacticsHealCardList = new List<GameObject>();
-        this.playableTacticsStrenghtCardList = new List<GameObject>();
-        this.playableTacticsAttackCardList = new List<GameObject>();
         this.attackableUnitList = new List<GameObject>();
         this.defendableUnitList = new List<GameObject>();
-        this.friendlyUnitList = new List<GameObject>();
     }
 
     public void manageMoves()
     {
         canAIMakeMove = false;
         playableCardList.Clear();
-        playableUnitCardList.Clear();
-        playableTacticsHealCardList.Clear();
-        playableTacticsStrenghtCardList.Clear();
-        playableTacticsAttackCardList.Clear();
         attackableUnitList.Clear();
         defendableUnitList.Clear();
-        friendlyUnitList.Clear();
         
         GameManager.Instance.currentPlayer.armymodel.armyCardsModel.showCardLists();
         if (GameManager.Instance.currentPlayer == GameManager.Instance.playerNorth)
         {
+            // create list of attackable friendly units 
             foreach (Transform child in GameManager.Instance.currentPlayer.dropZoneVisual.dropAreaImage.transform)
             {
                 Card cardInModel =
                     GameManager.Instance.currentPlayer.armymodel.armyCardsModel.findCardInFrontByID(
                         child.GetComponent<IDAssignment>().uniqueId);
-//                Debug.Log("Child: " + child.GetComponent<CardDisplayLoader>().cardNameText.text);
-//                Debug.Log("child ID : " + child.GetComponent<IDAssignment>().uniqueId);
-//                Debug.Log("CardsInModel: " + cardInModel);
                 if (cardInModel.currentAttacksPerTurn > 0 && cardInModel.isAbleToAttack)
                 {
                     attackableUnitList.Add(child.gameObject);
                 }
             }
 
-
+            // create list of defendable enemy units 
             foreach (Transform child in GameManager.Instance.otherPlayer.dropZoneVisual.dropAreaImage.transform)
             {
                 Card cardInModel =
@@ -76,7 +58,7 @@ public class AIManager : MonoBehaviour
             }
             
             
-            
+            // create list of playable cards 
             foreach (Transform child in GameManager.Instance.currentPlayer.handViewVisual.transform)
             {
                 CardVisualStateEnum cardDetailedType = child.GetComponent<CardDisplayLoader>().cardDetailedType;
@@ -85,17 +67,12 @@ public class AIManager : MonoBehaviour
                 if (int.Parse(child.GetComponent<CardDisplayLoader>().cardMoneyText.text.ToString()) <=
                     GameManager.Instance.currentPlayer.resourcesCurrent)
                 {
-                    Debug.Log("cardDetailedType: " + cardDetailedType);
-                    Debug.Log("cardDetailedType is UnitCard: " + (cardDetailedType == CardVisualStateEnum.UnitCard));
                     // ...add to playable card list if card will have any effect (heal, strenghten or attack any Unit) 
                     switch(child.GetComponent<CardDisplayLoader>().cardDetailedType)
                     {
                         case CardVisualStateEnum.UnitCard:
                         {
-                            Debug.Log("inside switch unit");
                             playableCardList.Add(child.gameObject);
-                            
-                            Debug.Log("playableCount " + playableCardList.Count);
                             break;
                         }
                         case CardVisualStateEnum.TacticsAttackAll:
@@ -116,26 +93,16 @@ public class AIManager : MonoBehaviour
                             {
                                 playableCardList.Add(child.gameObject);
                             }
-                            playableTacticsStrenghtCardList.Add(child.gameObject);
                             break;
                         }
                         default:
-                            Debug.Log("inside default");
+                            Debug.Log("default");
                             break;
                         
                     }
                 }
                 
             }
-
-
-            
-
-            Debug.Log("playableCount " + playableCardList.Count);
-
-            Debug.Log("attackableCount " + attackableUnitList.Count);
-
-            Debug.Log("defendableCount " + defendableUnitList.Count);
 
             if (playableCardList.Count <= 0 && attackableUnitList.Count <= 0)
             {
@@ -168,6 +135,18 @@ public class AIManager : MonoBehaviour
                     tacticsBonusOneAI(playableCardList[playableCardRandom],
                         defendableUnitList[defendableUnitRandom]);
                 }
+                else if (playableCardList[playableCardRandom].GetComponent<CardDisplayLoader>().cardDetailedType ==
+                         CardVisualStateEnum.TacticsHealAll)
+                {
+                    tacticsBonusAllAI(playableCardList[playableCardRandom],
+                        GameManager.Instance.currentPlayer.dropZoneVisual.gameObject);
+                }
+                else if (playableCardList[playableCardRandom].GetComponent<CardDisplayLoader>().cardDetailedType ==
+                         CardVisualStateEnum.TacticsAttackAll)
+                {
+                    tacticsBonusAllAI(playableCardList[playableCardRandom],
+                        GameManager.Instance.otherPlayer.dropZoneVisual.gameObject);
+                }
             }
             else if (attackableUnitList.Count > 0 && defendableUnitList.Count > 0)
             {
@@ -191,7 +170,7 @@ public class AIManager : MonoBehaviour
 
     IEnumerator endTurnAI()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
         GameManager.Instance.endTurnButtonManager.TimerStop();
     }
 
@@ -208,11 +187,9 @@ public class AIManager : MonoBehaviour
         eventDataDrag.position = cardToMove.transform.position;
 
         cardToMove.GetComponent<Draggable>().OnBeginDrag(eventDataDrag);
-//                playableCardList[0].transform.DOMove(GameManager.Instance.currentPlayer.dropZoneVisual.transform.position, 1).SetEase(Ease.OutQuint, 0.5f, 0.1f);
         eventDataDrag.pointerDrag = cardToMove;
         GameManager.Instance.currentPlayer.dropZoneVisual.OnDrop(eventDataDrag);
         cardToMove.GetComponent<Draggable>().OnEndDrag(eventDataDrag);
-        playableCardList.Clear();
     }
 
     public void unitAttacksEnemyUnitAI(GameObject attackingUnit, GameObject defendingUnit)
@@ -221,14 +198,11 @@ public class AIManager : MonoBehaviour
         eventDataDrag.position = attackingUnit.transform.position;
 
         attackingUnit.GetComponent<Attackable>().OnBeginDrag(eventDataDrag);
-//                playableCardList[0].transform.DOMove(GameManager.Instance.currentPlayer.dropZoneVisual.transform.position, 1).SetEase(Ease.OutQuint, 0.5f, 0.1f);
         eventDataDrag.pointerDrag = attackingUnit;
         defendingUnit.GetComponent<Defendable>().OnDrop(eventDataDrag);
         eventDataDrag.position = defendingUnit.transform.position;
         attackingUnit.GetComponent<Attackable>().initialPosition = attackingUnit.transform.position;
         attackingUnit.GetComponent<Attackable>().OnEndDrag(eventDataDrag);
-        attackableUnitList.Clear();
-        defendableUnitList.Clear();
     }
 
     public void tacticsBonusOneAI(GameObject attackingUnit, GameObject defendingUnit)
@@ -237,47 +211,37 @@ public class AIManager : MonoBehaviour
         eventDataDrag.position = attackingUnit.transform.position;
 
         attackingUnit.GetComponent<Attackable>().OnBeginDrag(eventDataDrag);
-//                playableCardList[0].transform.DOMove(GameManager.Instance.currentPlayer.dropZoneVisual.transform.position, 1).SetEase(Ease.OutQuint, 0.5f, 0.1f);
         eventDataDrag.pointerDrag = attackingUnit;
         defendingUnit.GetComponent<Defendable>().OnDrop(eventDataDrag);
         eventDataDrag.position = defendingUnit.transform.position;
         attackingUnit.GetComponent<Attackable>().initialPosition = attackingUnit.transform.position;
         attackingUnit.GetComponent<Attackable>().OnEndDrag(eventDataDrag);
-        attackableUnitList.Clear();
-        defendableUnitList.Clear();
     }
 
-    public void tacticsBonusAllAI(GameObject attackingUnit, GameObject defendingUnit)
+    public void tacticsBonusAllAI(GameObject attackingUnit, GameObject dropZoneAreObject)
     {
         PointerEventData eventDataDrag = new PointerEventData(EventSystem.current);
         eventDataDrag.position = attackingUnit.transform.position;
 
         attackingUnit.GetComponent<Attackable>().OnBeginDrag(eventDataDrag);
-//                playableCardList[0].transform.DOMove(GameManager.Instance.currentPlayer.dropZoneVisual.transform.position, 1).SetEase(Ease.OutQuint, 0.5f, 0.1f);
         eventDataDrag.pointerDrag = attackingUnit;
-        defendingUnit.GetComponent<Defendable>().OnDrop(eventDataDrag);
-        eventDataDrag.position = defendingUnit.transform.position;
+        dropZoneAreObject.GetComponent<DropZone>().OnDrop(eventDataDrag);
+        eventDataDrag.position = dropZoneAreObject.GetComponent<DropZone>().dropAreaImage.transform.position;
         attackingUnit.GetComponent<Attackable>().initialPosition = attackingUnit.transform.position;
         attackingUnit.GetComponent<Attackable>().OnEndDrag(eventDataDrag);
-        attackableUnitList.Clear();
-        defendableUnitList.Clear();
     }
 
     public void unitAttacksEnemyHeroAI(GameObject attackingUnit, GameObject defendingHero)
     {
-        Debug.Log("attack Hero");
         PointerEventData eventDataDrag = new PointerEventData(EventSystem.current);
         eventDataDrag.position = attackingUnit.transform.position;
 
         attackingUnit.GetComponent<Attackable>().OnBeginDrag(eventDataDrag);
-//                playableCardList[0].transform.DOMove(GameManager.Instance.currentPlayer.dropZoneVisual.transform.position, 1).SetEase(Ease.OutQuint, 0.5f, 0.1f);
         eventDataDrag.pointerDrag = attackingUnit;
         defendingHero.GetComponent<Defendable>().OnDrop(eventDataDrag);
         eventDataDrag.position = defendingHero.transform.position;
         attackingUnit.GetComponent<Attackable>().initialPosition = attackingUnit.transform.position;
         attackingUnit.GetComponent<Attackable>().OnEndDrag(eventDataDrag);
-        attackableUnitList.Clear();
-        defendableUnitList.Clear();
     }
 
     public void attackUnitOrHeroAI(GameObject attacker, GameObject defender)
