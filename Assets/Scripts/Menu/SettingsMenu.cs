53f28aefb34ecdbe3cc77d5b6ef7fbf0d20976db
Cristopher-91 <k.piasecki91@gmail.com>
2018-12-03 14:43:55 +0100

    Setts holder works - need to atribute general setts to GameManager

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Toggle arAvailable;
+    public Toggle asrAvailable;
+    public Toggle aIPlayerCardsSeen;
+    public Toggle musicinGamePlaying;
+
+    void Start()
+    {
+        if (SettsHolder.instance.isARAvailable == false) arAvailable.isOn = false;
+        if (SettsHolder.instance.isASRAvailable == false) asrAvailable.isOn = false;
+        if (SettsHolder.instance.AIPlayerCardsSeen == false) aIPlayerCardsSeen.isOn = false;
+        if (SettsHolder.instance.isMusicInGamePlaying == false) musicinGamePlaying.isOn = false;
+
+        arAvailable.onValueChanged.AddListener(delegate { SettsHolder.instance.changeARAvailable(); });
+        asrAvailable.onValueChanged.AddListener(delegate { SettsHolder.instance.changeASRAvailable(); });
+        aIPlayerCardsSeen.onValueChanged.AddListener(delegate { SettsHolder.instance.changeAIPlayerCardsSeen(); });
+        musicinGamePlaying.onValueChanged.AddListener(delegate { SettsHolder.instance.changeMusicInGamePlaying(); });
+    }
+
 }
