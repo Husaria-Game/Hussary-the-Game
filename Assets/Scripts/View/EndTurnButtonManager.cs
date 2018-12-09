db0a6725e114a9f8a1bfd33f1d77c80e247e7019
G-Captain <grzekanowski@gmail.com>
2018-12-09 23:16:53 +0100

    H-46 deleted timer functionality, adjusted fight for late turn damage calculation

--- Assets/Scripts/View/EndTurnButtonManager.cs
+++ Assets/Scripts/View/EndTurnButtonManager.cs
@@ -10,56 +10,24 @@ public class EndTurnButtonManager : MonoBehaviour
     public Text timerText;
     public Text buttonText;
 
-    private bool isCounting = false;
     private bool isARSceneActive = false;
     private float timerCountdown;
     private float timeLeft;
-    private const float TIME = 30;  //Time per round in seconds;
-
-    void Update()
-    {
-        if (isCounting)
-        {
-            timeLeft -= Time.deltaTime;
-            timerText.text = ToString();
-
-            if (isARSceneActive)
-            {
-                timeLeft += 10;
-                isARSceneActive = false;
-            }
-
-            if (timeLeft <= 0)
-            {
-                TimerStop();
-            }
-        }
-    }
 
     public void TimerStart()
     {
-        timeLeft = TIME;
         if(gameManager.typeOfEnemy == GameMode.Computer && gameManager.currentPlayer == gameManager.playerNorth)
         {
-            Debug.Log("Tura computera");
             ButtonDisable();
         }
         else
         {
             InitialButtonBlock(5f);
         }
-        StartCoroutine(TimerStartWithDelay());
-    }
-
-    IEnumerator TimerStartWithDelay()
-    {
-        yield return new WaitForSeconds(2f);
-        isCounting = true;
     }
 
     public void TimerStop()
     {
-        isCounting = false;
         SRS.StopCoroutineIfTurnButtonClicked();
         gameManager.nextTurn();
     }
@@ -91,16 +59,6 @@ public class EndTurnButtonManager : MonoBehaviour
     public void ARSceneBecomesActive()
     {
         isARSceneActive = true;
-    }
-
-    public override string ToString()
-    {
-        int seconds = Mathf.RoundToInt(timeLeft);
-        string secondsText = (seconds).ToString();
-        if (secondsText.Length == 1)
-            secondsText = "0" + secondsText;
-
-        return string.Format("{0}", secondsText + "s.");
     }
 }
 
